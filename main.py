f40d986aa94da3ab9e8ebe54e7ec6862e63a4331
Rostyslav Rachynskyi <r.rostik2000@gmail.com>
2024-11-04 04:00:36 +0200

    feat: goit-pycore-hw-06

---
+++ main.py
@@ -0,0 +1,23 @@
+from address_book import AddressBook
+from models.record import Record
+
+if __name__ == "__main__":
+    book = AddressBook()
+
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
+    book.add_record(john_record)
+
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+
+    for name, record in book.data.items():
+        print(record)
+
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+    print(john)
+
+    book.delete("Jane")
---
+++ models/record.py
@@ -0,0 +1,35 @@
+from .name import Name
+from .phone import Phone
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone_number):
+        phone = Phone(phone_number)
+        self.phones.append(phone)
+
+    def remove_phone(self, phone_number):
+        phone_to_remove = self.find_phone(phone_number)
+        if phone_to_remove:
+            self.phones.remove(phone_to_remove)
+        else:
+            raise ValueError("Phone number not found.")
+
+    def edit_phone(self, old_number, new_number):
+        phone_to_edit = self.find_phone(old_number)
+        if phone_to_edit:
+            self.remove_phone(old_number)
+            self.add_phone(new_number)
+        else:
+            raise ValueError("Phone number not found.")
+
+    def find_phone(self, phone_number):
+        for phone in self.phones:
+            if phone.value == phone_number:
+                return phone
+        return None
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
